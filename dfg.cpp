2d79465e3367538271124cca5d91b3e2c8355e6d
233806487+kodazuname@users.noreply.github.com <kodazuname@gmail.com>
2026-07-08 01:05:02 +0500

    First commit

---
+++ dfg.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	string s,a,b;
+    int a1,b1;
+    cin>>s;
+    if (s.length()==4){
+        // do something
+    }
+}
---
+++ main.js
@@ -0,0 +1,5 @@
+console.log("Hello, World!");
+let planets=["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
+for(let i=0;i<planets.length;i++){
+    console.log(planets[i]);
+}
